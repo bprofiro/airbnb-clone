1f0736e879d4bb231149abc3e539046b7e6691a1
Brenda Profiro <brendaprofiro@gmail.com>
2020-09-24 20:42:35 -0300

    added: Adicionar relacionamento entre os Models

--- airbnb-server/app/Models/User.js
+++ airbnb-server/app/Models/User.js
@@ -10,15 +10,15 @@ class User extends Model {
   static boot () {
     super.boot()
 
-    /**
-     * A hook to hash the user password before saving
-     * it to the database.
-     */
     this.addHook('beforeSave', async (userInstance) => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
-    })
+    });
+
+    properties () {
+      return this.hasMany('App/Models/Property')
+    }
   }
 
   /**
